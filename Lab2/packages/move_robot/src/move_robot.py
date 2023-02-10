721b05f234f5e558bae697fb2943ec244e3b1048
Tur4L <tural.bakhtiyarli.2003@gmail.com>
2023-02-10 11:34:39 -0700

    fixes

--- Lab2/packages/move_robot/src/move_robot.py
+++ Lab2/packages/move_robot/src/move_robot.py
@@ -1,69 +1,218 @@
 #!/usr/bin/env python3
-import numpy as np
-import os
+
+"""
+TODO: Shutdown nodes (send shutdown request to odometry node).
+"""
+
 import math
+import os
+import subprocess
+import time
+
+import numpy as np
 import rospy
-from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
-from duckietown_msgs.msg import Twist2DStamped, WheelEncoderStamped, WheelsCmdStamped
-from std_msgs.msg import Header, Float32
+from std_msgs.msg import Float32, String
+
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import WheelsCmdStamped
+from duckietown_msgs.srv import ChangePattern
+
 
 class DriverNode(DTROS):
-
     def __init__(self, node_name):
-        """Wheel Encoder Node
-        This implements basic functionality with the wheel encoders.
-        """
-
-        # Initialize the DTROS parent class
-        super(DriverNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
+        super(DriverNode, self).__init__(
+            node_name=node_name,
+            node_type=NodeType.GENERIC
+        )
         
-        # Get static parameters
-        self._veh_name = os.environ["VEHICLE_NAME"]
-        self._radius = rospy.get_param(f'/{self.veh_name}/kinematics_node/radius', 100)
+        # Static variables
+        self._veh = os.environ["VEHICLE_NAME"]
+        self._rate = rospy.Rate(100)
         self._robot_width_half = 0.05
-        self._rate = rospy.Rate(1)
 
-        # Subscribing to the wheel encoders
-        self.sub_encoder_ticks_left = rospy.Subscriber(f'/{self._veh_name}/left_wheel_encoder_node/tick', WheelEncoderStamped, self.cb_encoder_data,callback_args = "left",)
-        self.sub_encoder_ticks_right = rospy.Subscriber(f'/{self._veh_name}/right_wheel_encoder_node/tick', WheelEncoderStamped, self.cb_encoder_data,callback_args = "right")
-        self.sub_executed_commands = rospy.Subscriber(f'/{self._veh_name}/wheels_driver_node/wheels_cmd_executed', WheelsCmdStamped, self.cb_executed_commands)
+        # Values to keep track on
+        self.total_distance = 0.0   # Total distance progressed by robot
+        self.left_distance = 0.0    # Total distance progressed by left wheel
+        self.right_distance = 0.0   # Total distance progressed by right wheel
 
+        self.prev_left_dist = 0.0
+        self.theta = 0
+        
+        # Initial robot frame and world frame
+        self.robot_frame = np.zeros(3)
+        self.init_frame = np.zeros(3)
+        self.world_frame = np.zeros(3)
+        self.world_frame[:2] = 0.32
 
-        # Publishers
-        self.pub_vel = rospy.Publisher(f'/{self._veh_name}/wheels_driver_node/wheels_cmd',WheelsCmdStamped,queue_size = 10)
-        self.pub_executed_cmd= rospy.Publisher(f'/{self._veh_name}/wheels_driver_node/wheels_cmd_executed',WheelsCmdStamped,queue_size = 10)
+        # Service client
+        rospy.wait_for_service(f"/{self._veh}/led_emitter_node/set_custom_pattern")
+        
+        self.srv_led = rospy.ServiceProxy(
+            f"/{self._veh}/led_emitter_node/set_pattern",
+            ChangePattern
+        )
 
-        self.log("Initialized")
-
-
-        #Robot values
-        self.left_wheel_distance = 0.0
-        self.right_wheel_distance = 0.0
-        self.robot_distance = 0.0
-        self.total_ang = 0.0
-
+        # Publisher
+        self.pub_vel = rospy.Publisher(
+            f"{self._veh}/wheels_driver_node/wheels_cmd",
+            WheelsCmdStamped,
+            queue_size=10
+        )
+        self.pub_executed_cmd = rospy.Publisher(
+            f"/{self._veh}/wheels_driver_node/wheels_cmd_executed",
+            WheelsCmdStamped,
+            queue_size=10
+        )
+        # self.pub_odometry_log = rospy.Publisher(
+        #     f"/{self._veh}/driver_node/log",
+        # )
+        
+        # Subscribers
+        self.delta_dist_left = rospy.Subscriber(
+            f"{self._veh}/odometry_node/left_wheel_delta",
+            Float32,
+            self.cb_param_update,
+            callback_args="left"
+        )
+        self.delta_dist_right = rospy.Subscriber(
+            f"{self._veh}/odometry_node/right_wheel_delta",
+            Float32,
+            self.cb_param_update,
+            callback_args="right"
+        )
+    
     def reset_variables(self):
-        self.left_wheel_distance = 0.0
-        self.right_wheel_distance = 0.0
-        self.robot_distance = 0.0
+        """Reset all tracking variables to 0.
+        
+        Arguments
+        ---------
+        None
+        
+        Returns
+        -------
+        None
+        """
+        self.total_distance = 0.0
+        self.left_distance = 0.0
+        self.right_distance = 0.0
 
-    def cb_param_update(self,msg,wheel):
+    def set_led_color(self, pattern):
+        """Set LED pattern to the given desired color.
+        
+        Aruguments
+        ----------
+        pattern: str
+            String representing the color. By convention, it is fully capitalized.
+        """
+        msg = String()
+        msg.data = pattern
+        self.srv_led(msg)
+
+    def get_inv_rot_matrix(self, theta):
+        """Get the inverse rotation matrix for world frame.
+        """
+        return np.array([[np.cos(theta), -np.sin(theta), 0],
+                         [np.sin(theta), np.cos(theta), 0],
+                         [0, 0, 1]])
+        
+    def to_init_frame(self, theta):
+        """Convert robot frame to world frame."""
+        self.init_frame = self.get_inv_rot_matrix(theta).dot(self.robot_frame)
+        self.init_frame[2] %= (2 * math.pi)
+
+    def to_world_frame(self):
+        self.world_frame[0] += self.init_frame[1]
+        self.world_frame[1] += self.init_frame[0]
+        self.world_frame[2] = (self.init_frame[2] - (math.pi/2)) % (2*math.pi)
+
+    def cb_param_update(self, msg, wheel):
+        """Update distance parameters based on the subscriber's feedback.
+        
+        Arguments
+        ---------
+        msg: Float32
+            Change of distance on either wheel.
+        wheel: str
+            Indicator of which wheel has been called. ["left", "right"]
+        """
+        assert wheel in ["left", "right"]
 
         if wheel == "right":
-            self.right_wheel_distance += msg.data
-            self.total_dsitance = (self.left_wheel_distance + self.right_wheel_distance)/2
-            self.total_ang = (abs(self.left_wheel_distance) + abs(self.right_wheel_distance))/(2* self._robot_width_half)
+            # print("right", self.right_distance)
+            self.right_distance += msg.data
+            self.total_distance = (abs(self.left_distance) + abs(self.right_distance)) / 2.
+            self.robot_frame += np.array([(msg.data + self.prev_left_dist)/2., 0, (msg.data - self.prev_left_dist) / (2. * self._robot_width_half)])
+            self.robot_frame[2] %= (2 * math.pi)
+            self.to_init_frame(self.theta)
+            self.to_world_frame()
         else:
-            self.left_wheel_distance += msg.data
-
-    def send_msg(self,msg):
-
+            self.left_distance += msg.data
+            self.prev_left_dist = msg.data
+            # print("left", self.prev_left_dist)
+    
+    def send_msg(self, msg):
+        """Send the message indicating velocity to suitable topic.
+        
+        Arguments
+        ---------
+        msg: WheelsCmdStamped
+            Velocity message that we are feeding into a topic.
+        """
         self.pub_vel.publish(msg)
         self.pub_executed_cmd.publish(msg)
 
+    def stop(self):
+        """Method to stop the robot's movement."""
+        msg = WheelsCmdStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.vel_left = 0.0
+        msg.vel_right = 0.0
 
-if __name__ == '__main__':
-    node = DriverNode(node_name='driver_node')
-    # Keep it spinning to keep the node alive
-    rospy.spin()
-    rospy.loginfo("wheel_encoder_node is up and running...")
+        self.send_msg(msg)
+
+    def move(self, distance, vel_left=0.4, vel_right=0.4, offset=0):
+        """Method to move the robot in straight line for desired distance.
+        
+        Arguments
+        ---------
+        distance: float
+            Desired distance for robot to move straight. In meters.
+        vel_left: float
+            Velocity of left wheel.
+        vel_right: float
+            Velocity of right wheel.
+        """
+        # assert (vel_left > 0.0 and vel_right > 0.0) or (vel_left < 0.0 and vel_right < 0.0)
+        
+        # Reset the variables
+        self.reset_variables()
+        
+        # Construct message
+        msg = WheelsCmdStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.vel_left = vel_left
+        msg.vel_right = vel_right
+        
+        # Move until distance reaches `distance`
+        while abs(self.total_distance) < abs(distance) - offset:
+            # print(self.init_frame)
+            # print(self.world_frame)
+            msg.header.stamp = rospy.Time.now()
+            self.send_msg(msg)
+            self._rate.sleep()
+        
+        # Stop after reaching
+        self.stop()
+
+    def shutdown_hook(self):
+        print("Shutting down a Driver node...")
+
+        # Send signal to kill odometry node
+        subprocess.run(["rosnode", "kill", "odometry"])
+
+        self.set_led_color("LIGHT_OFF")
+
+if __name__ == "__main__":
+    driver = DriverNode("driver_node")
+
+    rospy.on_shutdown(driver.shutdown_hook)
